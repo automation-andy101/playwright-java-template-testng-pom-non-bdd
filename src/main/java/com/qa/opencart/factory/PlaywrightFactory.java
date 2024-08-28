8fedd519aa444fc4208b9f190d58e502deaed079
automation.andy101 <automation.andy101@gmail.com>
2024-08-28 21:51:28 +0100

    Added Extent Reporting

--- src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -6,7 +6,9 @@ import com.microsoft.playwright.BrowserType.LaunchOptions;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Properties;
+import java.nio.file.Paths;
 
 public class PlaywrightFactory {
     Playwright playwright;
@@ -14,6 +16,27 @@ public class PlaywrightFactory {
     BrowserContext browserContext;
     Page page;
     Properties properties;
+
+    private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+    private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+    private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
+    private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+
+    public static Playwright getPlaywright() {
+        return tlPlaywright.get();
+    }
+
+    public static Browser getBrowser() {
+        return tlBrowser.get();
+    }
+
+    public static BrowserContext getBrowserContext() {
+        return tlBrowserContext.get();
+    }
+
+    public static Page getPage() {
+        return tlPage.get();
+    }
 
     /**
      * This method is used to initialise the browser used to test the application
@@ -24,31 +47,40 @@ public class PlaywrightFactory {
     public Page initBrowser(Properties properties) {
         System.out.println("Browser name is : " + properties.getProperty("browser"));
 
-        playwright = Playwright.create();
+//        playwright = Playwright.create();
+        tlPlaywright.set(Playwright.create());
 
         switch(properties.getProperty("browser").toLowerCase()) {
             case "chromium":
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+//                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+                tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless")))));
                 break;
             case "chrome":
-                browser = playwright.chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+//                browser = playwright.chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+                tlBrowser.set(getPlaywright().chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(Boolean.parseBoolean(properties.getProperty("headless")))));
                 break;
             case "firefox":
-                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+//                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+                tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless")))));
                 break;
             case "safari":
-                browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+//                browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless"))));
+                tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(properties.getProperty("headless")))));
                 break;
             default:
                 System.out.println("Please pass a supported browser type......");
                 break;
         }
 
-        browserContext = browser.newContext();
-        page = browserContext.newPage();
-        page.navigate(properties.getProperty("url"));
+//        browserContext = browser.newContext();
+//        page = browserContext.newPage();
+//        page.navigate(properties.getProperty("url"));
 
-        return page;
+        tlBrowserContext.set(getBrowser().newContext());
+        tlPage.set(getBrowserContext().newPage());
+        getPage().navigate(properties.getProperty("url").trim());
+
+        return getPage();
     }
 
     /**
@@ -68,6 +100,19 @@ public class PlaywrightFactory {
         }
 
         return properties;
+    }
 
+    /**
+     * take screenshot
+     *
+     */
+    public static String takeScreenshot() {
+        String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+        //getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+
+        byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+        String base64Path = Base64.getEncoder().encodeToString(buffer);
+
+        return base64Path;
     }
 }
